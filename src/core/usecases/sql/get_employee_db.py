970978ccf36fd0cea10ced6dafc5afe6e2bbea36
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-12 19:39:38 +0100

    usecase: get employee method

---
+++ src/core/usecases/sql/get_employee_db.py
@@ -0,0 +1,14 @@
+from typing import Type
+
+from src.core.repositories import IEmployeeRepository
+
+class GetEmployeeDb:
+    
+    def __init__(self, employee_repository_interface: Type[IEmployeeRepository]):
+        self.__repository_interface = employee_repository_interface()
+    
+
+    def get(self):
+        """ Returns the employee with location"""
+
+        return self.__repository_interface.get_employee()
