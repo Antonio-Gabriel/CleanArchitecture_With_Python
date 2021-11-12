ad376ff6289e6ada0d78fe314c8076e242ea758c
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-12 10:15:41 +0100

    repository: create get employee method

---
+++ src/core/usecases/get_employee.py
@@ -0,0 +1,12 @@
+from typing import Type
+
+from src.core.repositories import IEmployeeRepository
+
+
+class GetEmployee:
+    
+    def __init__(self, employee_repository_interface: Type[IEmployeeRepository]):
+        self.__employee_repository = employee_repository_interface()
+
+    def execute(self):
+        return self.__employee_repository.get_employees()
