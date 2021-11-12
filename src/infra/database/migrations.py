3c65661191a88ea32dfe01aca9179cc2bfc92cc6
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-12 16:11:21 +0100

    database: create migrations

---
+++ src/infra/database/migrations.py
@@ -0,0 +1,39 @@
+from src.infra.database.database import get_database_connection
+
+def make_migrations():
+    """
+    Creates a table ready to accept our data.
+
+    write code that will execute the given sql statement
+    on the database
+    """
+    connection = get_database_connection()
+
+    statement_location = """        
+        CREATE TABLE IF NOT EXISTS location(
+            id integer primary key not null,
+            district text not null,
+            city text not null,
+            road text not null
+        )
+    """ 
+
+    connection.execute(statement_location)
+
+    statement_employee = """
+        CREATE TABLE IF NOT EXISTS employee(
+            id integer primary key not null,
+            location integer not null,
+            name text NOT NULL,
+            email text NOT NULL,
+            phone integer,
+
+            foreign key (location) 
+                references location(id)
+        )
+    """
+
+    connection.execute(statement_employee)
+
+    connection.commit()
+    connection.close()
