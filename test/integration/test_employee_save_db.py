b19bb6054876afcae08e091a0b060e9d86569118
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-12 19:29:36 +0100

    test:  save_employee test

---
+++ test/integration/test_employee_save_db.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+
+from src.core.dto import IEmployeeRequestDto
+from src.infra.repository.sql import EmployeeRepository
+from src.core.usecases.sql import SaveEmployeeDb
+
+from src.core.entities import Location
+
+class EmployeeSaveDb(TestCase):
+
+    def test_save_employee_db(self):
+
+        employee_data = SaveEmployeeDb(EmployeeRepository)
+        response = employee_data.execute(
+            IEmployeeRequestDto(
+                    name='Ag', 
+                    email='test@example.com', 
+                    phone=998987688,
+                    location= Location(
+                        district="Newyork",
+                        city='Newyork',
+                        road="NS"
+                    )
+                )
+            )
+
+        print(response)
+        
+        self.assertTrue(True)
