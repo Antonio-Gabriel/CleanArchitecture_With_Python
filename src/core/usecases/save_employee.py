49ac40a2cf99c5a6c072fb7515c5fe8e10b0abf2
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-12 01:01:40 +0100

    repositoty: finish create_employee

--- src/core/usecases/save_employee.py
+++ src/core/usecases/save_employee.py
@@ -1,28 +1,42 @@
+#!python3
 from typing import Type
-from core.entities.employee import Employee
+from src.core.entities import *
 
-from core.repositories import IEmployeeRepository
-from src.core.entities.location import Location
+from src.core.repositories import IEmployeeRepository
+
 
 class SaveEmployee:
 
-    def __init__(self):
-        self.employee_repository = IEmployeeRepository()
+    def __init__(self, employee_repository_interface: Type[IEmployeeRepository]):
+        self.__employee_repository = employee_repository_interface()              
 
-    def execute(self, **kwargs):
+
+    def execute(self, **kwargs) -> Employee:       
+
+        if self.__is_null_or_empty(**kwargs): 
+            raise Exception("Error on Object, have a empty value")
 
         employee_data = Employee(
             name=kwargs["name"],
             email=kwargs["email"],
             phone=kwargs["phone"],
             location= Location(
-                    district=kwargs["district"],
-                    city=kwargs["city"],
-                    road=kwargs["road"]
+                district=kwargs["district"],
+                road=kwargs["road"],
+                city=kwargs["city"]
                 )
             )
-
-        if employee_data.isValidPhoneNumber: 
-            raise Exception("The value of number id not valied!.")
+                    
+        if not employee_data.isValidPhoneNumber(**{
+            "phone": kwargs["phone"]
+        }): 
+            raise Exception("The value of number is not valied!.")
+                
+        self.__employee_repository.save_employee(employee_data)  
+        return employee_data.show_current_employee             
         
-        self.employee_repository.save_employee(employee_data)        
+             
+    def __is_null_or_empty(self, **kwargs) -> bool:
+        for data in kwargs:
+            if data == "" or None:
+                return False
