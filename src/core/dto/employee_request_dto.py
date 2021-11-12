3fb3ba173cdda0a1af61c6fb62653d92966b908c
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-12 17:24:58 +0100

    dto: add validator for required data

---
+++ src/core/dto/employee_request_dto.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass, fields
+
+from src.core.entities import Location
+
+@dataclass(frozen=True)
+class IEmployeeRequestDto:
+    name: str
+    email: str
+    phone: int
+    location: Location
+
+    def __post_init__(self):
+        for field in fields(self):
+            value = getattr(self, field.name)
+            if not isinstance(value, field.type):
+                raise ValueError(
+                    f"Expected {field.name} to be {field.type} but got {repr(value)}"
+                    )
+                    
+        self.__required_args()
+
+
+    def __required_args(self):
+        for field in fields(self):
+            value = getattr(self, field.name)
+            if value == "" or None:
+                raise TypeError(
+                    f"Value {field.name} is required"
+                )
