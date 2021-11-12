9a8344c12869acd8356bf2d65923b889a125616e
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-12 01:06:05 +0100

    Tdd of employee

--- test/unit/test_save_employee.py
+++ test/unit/test_save_employee.py
@@ -1,16 +1,35 @@
 from unittest import TestCase
 
 from src.core.entities.employee import Employee
-#from src.core.usecases.save_employee import SaveEmployee
+from src.core.entities.location import Location
+
+from src.core.usecases import SaveEmployee
+from src.infra.repository import EmployeeRepositoryMemory
+
+
 
 class TestSaveEmployee(TestCase):
     
     def test_employee_entitty(self):
 
-        Employee("Ag", "ag@example.com", 998987888)        
+        Employee("Ag", "ag@example.com", 998987888, 
+        Location("Newyork","Newyork", "US"))      
+        
         self.assertTrue(True)
     
     def test_save_employee_with_location(self):
+
+        employee_data = SaveEmployee(EmployeeRepositoryMemory)
+        employee = employee_data.execute(
+            **{
+                "name": "Ag",
+                "email": "ag@example.com",
+                "phone": 998987890,
+                "district": "New York",
+                "city": "New York",
+                "road": "New York"
+            }
+        )
         
-        
+        print(employee)
         self.assertTrue(True)
