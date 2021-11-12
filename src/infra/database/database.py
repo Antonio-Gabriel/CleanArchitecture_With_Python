7aa44b8ce48b7b64c404f662fe2f918f2455375f
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-12 16:11:02 +0100

    database: local database configuration

---
+++ src/infra/database/database.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+DATABASE_NAME = "db_employee.db"
+
+def get_database_connection():
+    conn = sqlite3.connect(DATABASE_NAME)
+    conn.execute("PRAGMA foreign_keys = 1")
+    return conn
