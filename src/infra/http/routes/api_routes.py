5b60741b6bb51b3e71bf1c82294dfa98c9dd4983
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-19 20:49:02 +0100

    route: create routes with adapter

--- src/infra/http/routes/api_routes.py
+++ src/infra/http/routes/api_routes.py
@@ -1,13 +1,18 @@
 from flask import Blueprint, jsonify, request
-from src.controller.employee_controller import EmployeeController
+from src.controller import EmployeeController
+from src.adapter import FlaskAdapter
 
 api_routes_bp = Blueprint('api_routes', __name__)
+
 
 @api_routes_bp.route('/', methods=['GET'])
 def hello():
     """ first route """
 
-    employee_controller = EmployeeController()
-    response = employee_controller.route(request)
+    # employee_controller = EmployeeController()
+    # response = employee_controller.route(request)
 
-    return jsonify({"msg": "hello world", "data": response.body})
+    adapter = FlaskAdapter()
+    response = adapter.execute(request, EmployeeController)
+
+    return jsonify({"status_code": response.status_code, "data": response.body})
