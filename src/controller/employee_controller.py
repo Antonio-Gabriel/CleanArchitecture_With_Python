04bdd03355067697d095e31aa805facb30949d6d
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-19 22:02:00 +0100

    controller: create employee

--- src/controller/employee_controller.py
+++ src/controller/employee_controller.py
@@ -1,13 +1,47 @@
 from typing import Type
+from src.core.entities import Location
+from src.infra.errors.http_errors import HttpErrors
 from src.infra.helpers import HttpRequest, HttpResponse
 from src.main import IRouter
+
+from src.core.usecases.sql import SaveEmployeeDb
+from src.infra.repository.sql import EmployeeRepository
+from src.core.dto import IEmployeeRequestDto
+
 
 class EmployeeController(IRouter):
 
     def __init__(self):
-        pass
+        self.__save_employee_use_case = SaveEmployeeDb(EmployeeRepository)
 
     def route(self, http_request: Type[HttpRequest]) -> HttpResponse:
         """ Employee route for get """
-        
-        return HttpResponse(status_code=200, body={"name": http_request.body["name"]})
+
+        if http_request.body or http_request.query:
+
+            response = self.__save_employee_use_case.execute(IEmployeeRequestDto(
+                name=http_request.body["name"],
+                email=http_request.body["email"],
+                phone=int(http_request.body["phone"]),
+                location=Location(
+                    city=http_request.body["city"],
+                    district=http_request.body["district"],
+                    road=http_request.body["road"]
+                )
+            ))
+
+            response_body = {
+                "name": response.name,
+                "email": response.email,
+                "phone": response.phone,
+                "location": {
+                    "district": response.location["district"],
+                    "city": response.location["city"],
+                    "road": response.location["road"],
+                },
+            }
+
+            return HttpResponse(status_code=200, body=response_body)
+
+        http_error = HttpErrors._400()
+        return HttpResponse(status_code=http_error["status_code"], body=http_error["body"])
