fc6d8065ab30246ab3c4170e7680fe142e86f332
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-19 19:35:27 +0100

    controller: employee controller

---
+++ src/controller/employee_controller.py
@@ -0,0 +1,11 @@
+from typing import Type
+from src.infra.helpers import HttpRequest, HttpResponse
+
+class EmployeeController:
+
+    def __init__(self):
+        pass
+
+    def route(self, http_request: Type[HttpRequest]) -> HttpResponse:
+
+        return HttpResponse(status_code=200, body={"name": "Herlander de Castro Bento"})
