591dc9408658f4c5f11832fee30ee42498bb2584
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-19 19:35:44 +0100

    routes: frist route

--- src/infra/http/routes/api_routes.py
+++ src/infra/http/routes/api_routes.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from src.controller.employee_controller import EmployeeController
 
 api_routes_bp = Blueprint('api_routes', __name__)
 
 @api_routes_bp.route('/', methods=['GET'])
 def hello():
-    return jsonify({"msg": "hello world", "data": {"name": "António Gabriel"}})
+
+    employee_controller = EmployeeController()
+    response = employee_controller.route(request)
+
+    return jsonify({"msg": "hello world", "data": response.body})
