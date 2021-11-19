1993c016e41509bdbade209af34201a0ef13d7da
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-19 22:02:34 +0100

    routes: implement het route

--- src/infra/http/routes/api_routes.py
+++ src/infra/http/routes/api_routes.py
@@ -5,11 +5,21 @@ from src.adapter import FlaskAdapter
 api_routes_bp = Blueprint('api_routes', __name__)
 
 
-@api_routes_bp.route('/', methods=['GET'])
-def hello():
+@api_routes_bp.route('/', methods=['POST'])
+def create():
     """ first route """
 
     adapter = FlaskAdapter()
     response = adapter.execute(request, EmployeeController())
-    
-    return jsonify({"status_code": response.status_code, "data": response.body})
+
+    return jsonify({"status_code": response.status_code, "data": [response.body]})
+
+
+@api_routes_bp.route('/employee', methods=['GET'])
+def get_employee_with_location():
+    """ return employee """
+
+    adapter = FlaskAdapter()
+    response = adapter.execute(request, EmployeeController())
+
+    return jsonify({"status_code": response.status_code, "data": [response.body]})
