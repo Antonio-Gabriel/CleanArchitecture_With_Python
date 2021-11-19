d3bec1c87d8f3b1185380793b018c02c0a14e77f
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-19 20:06:06 +0100

    routes: add comment

--- src/infra/http/routes/api_routes.py
+++ src/infra/http/routes/api_routes.py
@@ -5,6 +5,7 @@ api_routes_bp = Blueprint('api_routes', __name__)
 
 @api_routes_bp.route('/', methods=['GET'])
 def hello():
+    """ first route """
 
     employee_controller = EmployeeController()
     response = employee_controller.route(request)
