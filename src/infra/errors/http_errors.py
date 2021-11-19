f50eba7e2dce775d4fe6cf44ea9dfd69b9f05d2f
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-19 15:45:43 +0100

    errors: configure errors file

--- src/infra/errors/http_errors.py
+++ src/infra/errors/http_errors.py
@@ -7,7 +7,26 @@ class HttpErrors:
 
         return {"status_code": 400, "body": {"error": "Bad Request"}}
 
+    @staticmethod
     def _404():
         """ Http error 404 - Not Found """
 
         return {"status_code": 404, "body": {"error": "Not Found"}}
+
+    @staticmethod
+    def _409():
+        """ Http error 409 - Conflit """
+
+        return {"status_code": 409, "body": {"error": "Conflit"}}
+
+    @staticmethod
+    def _422():
+        """ Http error 422 - Unprocessable Entity """
+
+        return {"status_code": 422, "body": {"error": "Unprocessable Entity"}}
+
+    @staticmethod
+    def _500():
+        """ Http error 500 - Internal Server Error """
+
+        return {"status_code": 500, "body": {"error": "Internal Server Error"}}
